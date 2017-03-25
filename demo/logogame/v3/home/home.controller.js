faac5726ed74328fed1b982b7a8723fcc32f3c66
sgrillon <sgrillon@hotmail.fr>
2017-03-26 00:13:10 +0100

    remove scenario part from POC

--- demo/logogame/v3/home/home.controller.js
+++ demo/logogame/v3/home/home.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['UserService', 'ScenarioService', '$rootScope', '$scope', '$translate', '$cookieStore'];
-    function HomeController(UserService, ScenarioService, $rootScope, $scope, $translate, $cookieStore) {
+    HomeController.$inject = ['UserService', '$rootScope', '$scope', '$translate', '$cookieStore'];
+    function HomeController(UserService, $rootScope, $scope, $translate, $cookieStore) {
 
     	var vm = this;
         vm.user = null;
@@ -19,31 +19,11 @@
         };
         $scope.loadCurrentUser();
 
-        $scope.runScenarioId=0;
-        
-		$scope.getScenarios = function () {
-			ScenarioService.getScenarios($rootScope.language).then(
-		    function (response) {
-		        if (response.status == 200) {
-		            $scope.scenarios = response.data;
-		        }
-		    });
-		};
-		$scope.getScenarios($rootScope.language);
-		
-		$scope.getScenario = function (scenarioId) {
-			return ScenarioService.getScenario(scenarioId);
-		};
-		
 		$scope.changeLang = function () {
 			$cookieStore.put('logogame.language', $rootScope.language);
 			$translate.use($rootScope.language);
-			$scope.getScenarios($rootScope.language);
 		};
 		
-		$scope.runScenario = function () {
-			return ScenarioService.runScenario($scope.runScenarioId);
-        };
     }
 
 })();
