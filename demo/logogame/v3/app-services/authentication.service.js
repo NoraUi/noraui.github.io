fe09a36b6b1b7506618e816bc39130c29e0ca801
sgrillon <sgrillon@hotmail.fr>
2017-03-25 22:57:55 +0100

    change POST by GET (gitHub stub)

--- demo/logogame/v3/app-services/authentication.service.js
+++ demo/logogame/v3/app-services/authentication.service.js
@@ -16,7 +16,7 @@
         return service;
 
         function Login(userName, password, callback) {
-            $http.post('api/authenticate', { userName: userName, password: password })
+            $http.get('api/authenticate', { userName: userName, password: password })
                 .success(function (response) {
                     callback(response);
                 });
