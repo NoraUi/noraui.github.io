5ca175b6832d2674f2547c4cd22ff84d63a94d83
Stéphane GRILLON <sgrillon@hotmail.fr>
2018-06-06 21:56:32 +0200

    add jhipstersample for use a sample on a new robot

---
+++ demo/jhipstersample/app-services/user.service.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UserService', UserService);
+
+    UserService.$inject = ['$http'];
+    function UserService($http) {
+        var service = {};
+
+        service.GetById = GetById;
+        service.GetByUsername = GetByUsername;
+
+        return service;
+
+        function GetById(id) {
+            return $http.get('api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+        }
+
+        function GetByUsername(username) {
+            return $http.get('api/users/' + username).then(handleSuccess, handleError('Error getting user by username'));
+        }
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
